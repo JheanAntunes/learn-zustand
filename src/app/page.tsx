12ac572624934dbb9f1d3f7bf3df438b4a8e5c0c
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-10-23 09:44:20 -0300

    :sparkles:	feat: add Component Animais e Controls animais in home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import ControlsCount from "@/components/store/controls";
-import Count from "@/components/store/count";
+import Animais from "@/components/store/animais";
+import ControlsAnimais from "@/components/store/controls-animais";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-">
-      <Count />
-      <ControlsCount />
+      <Animais />
+      <ControlsAnimais />
     </div>
   );
 }
