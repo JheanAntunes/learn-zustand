792b91e16f21cca684ce7846796243bbb1cbe7c3
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-10-30 12:41:59 -0300

    :sparkles:	feat: add lib immer in store

--- src/hooks/store/use-store-animais.ts
+++ src/hooks/store/use-store-animais.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
 type State = {
   mae: number;
   pai: {
@@ -16,23 +17,30 @@ type Action = {
 
 type UseStoreAnimais = State & Action;
 
-export const useStoreAnimais = create<UseStoreAnimais>()((set) => ({
-  mae: 0,
-  pai: {
-    dogs: {
-      dog: {
-        children: 0,
-      },
-    },
-  },
-  addChildrenDog: () =>
-    set((state) => ({
-      pai: {
-        ...state.pai,
-        dogs: {
-          ...state.pai.dogs,
-          dog: { children: state.pai.dogs.dog.children + 1 },
+export const useStoreAnimais = create<UseStoreAnimais>()(
+  immer((set) => ({
+    mae: 0,
+    pai: {
+      dogs: {
+        dog: {
+          children: 0,
         },
       },
-    })),
-}));
+    },
+    // addChildrenDog: () =>
+    //   set((state) => ({
+    //     pai: {
+    //       ...state.pai,
+    //       dogs: {
+    //         ...state.pai.dogs,
+    //         dog: { children: state.pai.dogs.dog.children + 1 },
+    //       },
+    //     },
+    //   })),
+
+    addChildrenDog: () =>
+      set((state) => {
+        ++state.pai.dogs.dog.children;
+      }),
+  }))
+);
