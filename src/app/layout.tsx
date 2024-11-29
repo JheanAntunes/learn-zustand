ade5a0cb1983647500d2e60ef55fd112fad54de4
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-11-29 16:34:15 -0300

    add  Provider BearContext with prop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ProviderContextBear from "@/provider/provider-context-bear";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,11 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ProviderContextBear bears={10}>{children}</ProviderContextBear>
       </body>
     </html>
   );
